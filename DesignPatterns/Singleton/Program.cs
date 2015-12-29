fd846823a2a9c325f0f314a6e43ca4d37f5be8b9
Sergii Kovach <skovach@SILVA.softservecom.com>
2015-12-29 14:38:44 +0200

    Refactor Usings

--- DesignPatterns/Singleton/Program.cs
+++ DesignPatterns/Singleton/Program.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Singleton
+﻿namespace Singleton
 {
         public class SingletonDemo
         {
